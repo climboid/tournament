fab5b693a5b9c5a19a65f7710b67cfcade9b3d0c
Oscar Villarreal <oscarvillarreal14@gmail.com>
2015-06-13 06:58:52 -0400

    fixes to sql file

--- tournament.sql
+++ tournament.sql
@@ -8,8 +8,7 @@
 
 -- Cretes a table of players which will hold all of the players of the tournament
 -- Each player has an id and a name
-CREATE TABLE players(id SERIAL PRIMARY KEY,
-											name TEXT);
+CREATE TABLE players(id SERIAL PRIMARY KEY, name TEXT);
 
 -- Creates a table that holds all the matches that are held. For each match you have
 -- a winner and a user which will contain id corresponding to that person
